5ac50261a5681d79ba6aaf99f323e58c1051b9f7
Antonio Papa <antoniopapa1991@gmail.com>
2024-06-25 16:58:51 +0200

    7 - Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import ChatView from "@/views/ChatView.vue";
 import RegisterView from "@/views/RegisterView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,10 @@ const router = createRouter({
         {
             path: '/register',
             component: RegisterView
+        },
+        {
+            path: '/login',
+            component: LoginView
         },
         {
             path: '/',
