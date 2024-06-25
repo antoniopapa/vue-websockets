80fc20db5e6c461c76b74b23066d646a2bb73f42
Antonio Papa <antoniopapa1991@gmail.com>
2024-06-25 13:52:28 +0200

    4 - Socket.io

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <script setup>
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {ref} from "vue";
+import axios from "axios";
+import {io} from "socket.io-client";
+
+const socket = io('http://localhost:8080/', {});
+
+const message = ref('');
+const messages = ref([]);
+
+socket.on('message', (msg) => {
+  messages.value = [...messages.value, msg]
+})
+
+const submit = async () => {
+  await axios.post('http://localhost:8000/api/message', {
+    message: message.value
+  })
+
+  message.value = '';
+}
 </script>
 
 <template>
@@ -25,28 +45,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
         </div>
 
         <div id="conversation">
-          <div class="row pt-2">
+          <div class="row pt-2" v-for="msg of messages">
             <div class="col-6">
               <div class="alert alert-success d-inline-block" role="alert">
-                Hi
+                {{ msg }}
               </div>
             </div>
             <div class="col-6"></div>
           </div>
 
-          <div class="row pt-2">
-            <div class="col-6"></div>
-            <div class="col-6">
-              <div class="alert alert-primary d-inline-block float-end" role="alert">
-                Hello There
-              </div>
-            </div>
-          </div>
+          <!--          <div class="row pt-2">-->
+          <!--            <div class="col-6"></div>-->
+          <!--            <div class="col-6">-->
+          <!--              <div class="alert alert-primary d-inline-block float-end" role="alert">-->
+          <!--                Hello There-->
+          <!--              </div>-->
+          <!--            </div>-->
+          <!--          </div>-->
         </div>
 
-        <form id="reply" class="p-3 w-100">
+        <form id="reply" class="p-3 w-100" @submit.prevent="submit">
           <div class="input-group">
-            <input class="form-control" placeholder="Write a message"/>
+            <input class="form-control" placeholder="Write a message" v-model="message"/>
           </div>
         </form>
       </div>
